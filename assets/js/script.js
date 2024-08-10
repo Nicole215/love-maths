e2fd96f2ecd4e9663c7d6c78d16f8827a5b6e1bc
Nicole Burmann <daria215@gmail.com>
2024-08-10 09:03:41 +0000

    Add division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,8 @@ function runGame(gameType){
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     }
        else {
         alert(`Unknown game type: ${gameType}`);
@@ -88,7 +90,10 @@ function calculateCorrectAnswer(){
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
 
-    } else {
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+    }
+    else {
         alert(`Unimplmented operator ${operator}`);
         throw `Unimplemented operator ${operator}.Aborting!`;
     }
@@ -131,6 +136,9 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById('operator').textContent = "x";
 }
 
-function displayDivisionQuestion(){
-
+function displayDivisionQuestion(operand1, operand2){
+    operand1 = operand1 * operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 }
